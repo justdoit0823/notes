c5bf2cd19728700f04df436d44e0c110a745d8f3
justdoit <justdoit920823@gmail.com>
2015-05-28 18:33:02 +0800

    增加在git-emacs中检查新加的和修改的py文件.

--- lisp/emacs-plugin.el
+++ lisp/emacs-plugin.el
@@ -1,6 +1,35 @@
 ;; This is a collection of my own emacs lisp plugin.
 
 
+(defun git--check-python-syntax (filename)
+  "check python file syntax in git repository"
+  (unless (not (equal (file-name-extension filename) "py"))
+    (let
+	((ret (shell-command-to-string
+	       (concat "python3 /usr/local/bin/pyflakes "
+		       (git--get-top-dir) filename))))
+      (when (> (length ret) 0) (error ret)))))
+
+
+;; check added python file syntax
+(defun git--check-added ()
+  "check added files"
+  (dolist (filename
+	   (split-string
+	    (git--exec-string "diff" "--cached" "--name-only") "[\n]+"))
+    (git--check-python-syntax filename)))
+
+
+;; check modified python file syntax
+(defun git--check-modified ()
+  "check modified files"
+  (dolist (file (git--ls-files "-m"))
+    (let ((filename (git--fileinfo->name file)))
+      (git--check-python-syntax filename))))
+
+
+
+
 (defun py-check-with-git ()
   "check python modified code in git repository"
   (dolist (file (git--ls-files "-m"))
@@ -11,3 +40,6 @@
 		   (concat "python /usr/local/bin/pyflakes "
 			   (git--get-top-dir) filename))))
   	  (if (> (length ret) 0) (error ret) (message "pass check")))))))
+
+
+(provide 'emacs-plugin)
