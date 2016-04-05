615181ca61947fd8207aa712dbb740ea63487165
justdoit <justdoit920823@gmail.com>
2016-04-05 14:53:09 +0800

    增加用PIL给图片加水印的代码片段。

---
+++ python/code-sample/watermark.py
@@ -0,0 +1,27 @@
+
+
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance
+
+
+FONT = '~/Downloads/arial.ttf'
+
+
+def add_watermark(in_file, text, out_file='watermark.jpg', angle=23, opacity=0.25):
+    img = Image.open(in_file).convert('RGB')
+    watermark = Image.new('RGBA', img.size, (0,0,0,0))
+    size = 2
+    n_font = ImageFont.truetype(FONT, size)
+    n_width, n_height = n_font.getsize(text)
+    while n_width+n_height < watermark.size[0]:
+        size += 2
+        n_font = ImageFont.truetype(FONT, size)
+        n_width, n_height = n_font.getsize(text)
+    draw = ImageDraw.Draw(watermark, 'RGBA')
+    draw.text(((watermark.size[0] - n_width) / 2,
+               (watermark.size[1] - n_height) / 2),
+              text, font=n_font)
+    watermark = watermark.rotate(angle,Image.BICUBIC)
+    alpha = watermark.split()[3]
+    alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
+    watermark.putalpha(alpha)
+    Image.composite(watermark, img, watermark).save(out_file, 'JPEG')
