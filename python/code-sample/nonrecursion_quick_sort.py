c995a1cdce962247d271cb1d5c8518781f03cfba
余森彬 <yusenbin@laiye.com>
2016-11-15 14:52:42 +0800

    增加非递归版快速排序。

---
+++ python/code-sample/nonrecursion_quick_sort.py
@@ -0,0 +1,59 @@
+
+"""
+非递归quick sort。
+"""
+
+import random
+
+
+def partition(s, low, high):
+    """
+    切分函数。
+    """
+
+    if low == high:
+        return low
+
+    pivot = (low + high) // 2
+    pivot_value = s[pivot]
+    low_idx = low
+    s[pivot], s[high] = s[high], pivot_value
+    for idx in range(low, high):
+        if s[idx] <= pivot_value:
+            if idx > low_idx:
+                s[low_idx], s[idx] = s[idx], s[low_idx]
+
+            low_idx += 1
+
+    s[low_idx], s[high] = pivot_value, s[low_idx]
+    return low_idx
+
+
+def quick_sort(s_list):
+    """
+    非递归版quick sort。
+    """
+
+    low = 0
+    high = len(s_list) - 1
+    sort_partitions = {(low, high)}
+    while sort_partitions:
+        par_low, par_high = sort_partitions.pop()
+        pivot = partition(s_list, par_low, par_high)
+        if pivot > par_low:
+            sort_partitions.add((par_low, pivot - 1))
+        if pivot < par_high:
+            sort_partitions.add((pivot + 1, par_high))
+
+
+def main():
+
+    test_list = [random.randint(0, 10000) for i in range(1000)]
+    print(test_list)
+    quick_sort(test_list)
+    print(test_list)
+
+
+if __name__ == '__main__':
+
+    main()
