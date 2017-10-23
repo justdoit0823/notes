e348a7927323d74efbe9f62d6f24a155673497ed
余森彬 <yusenbin@laiye.com>
2017-10-23 10:50:12 +0800

    Add dynamic function call.

--- c/function-in-c.c
+++ c/function-in-c.c
@@ -1,14 +1,24 @@
 
 #include "stdio.h"
 
+typedef int (*foo_ptr) (void);
+
 
 int hello(const char * str){
-  printf("%s.\n", str);
+  printf("hello %s.\n", str);
+  return 0;
+}
+
+
+int foo(void){
+  printf("foo function.\n");
   return 0;
 }
 
 
 int main(int argc, char * argv[]){
+  foo_ptr f_ptr = foo;
   hello(argv[0]);
+  (*f_ptr)();
   return 0;
 }
