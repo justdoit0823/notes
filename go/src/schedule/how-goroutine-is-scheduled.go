a1a5faa84f06c78bbeb8c3706fece4b1fce29f2d
余森彬 <yusenbin@laiye.com>
2017-12-19 18:49:01 +0800

    Remove channel.

--- go/src/schedule/how-goroutine-is-scheduled.go
+++ go/src/schedule/how-goroutine-is-scheduled.go
@@ -12,17 +12,23 @@ import (
 
 func main(){
 
-	ch := make(chan int, 1)
 	fmt.Printf("Current goroutine num %d.\n", runtime.NumGoroutine())
 
 	go func(timeout int){
-		ch <- 1
 		time.Sleep(time.Duration(timeout) * time.Second)
 		fmt.Printf("This goroutine has slept %d seconds.\n", timeout)
 	}(3)
 
-	<- ch
 	fmt.Printf("Current goroutine num %d.\n", runtime.NumGoroutine())
 
-	for ;; {}
+	first_warning := true
+	s_time := time.Now().Unix()
+
+	for ;; {
+		if( first_warning && (time.Now().Unix() - s_time) > 3) {
+			fmt.Printf("%d seconds has already gone.\n", 3)
+			first_warning = false
+		}
+	}
+
 }
