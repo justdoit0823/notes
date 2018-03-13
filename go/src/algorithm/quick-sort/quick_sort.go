73aa966efc0399dcf34fd56bd60b86049307aa56
余森彬 <yusenbin@laiye.com>
2018-03-13 20:28:49 +0800

    Add quicksort algorithm written in go.

---
+++ go/src/algorithm/quick-sort/quick_sort.go
@@ -0,0 +1,74 @@
+
+// Algorithm quicksort in the go.
+
+
+package main
+
+import "fmt"
+import "math/rand"
+import "os"
+import "strconv"
+
+
+func partition(eleList []int, low, high int) (int) {
+	pivot := int((low + high) / 2)
+	pivotValue := eleList[pivot]
+
+	eleList[pivot], eleList[high] = eleList[high], eleList[pivot]
+
+	lowIndex := low
+	for i := low; i < high; i++ {
+		if eleList[i] < pivotValue {
+			eleList[lowIndex], eleList[i] = eleList[i], eleList[lowIndex]
+			lowIndex++
+		}
+	}
+
+	eleList[lowIndex], eleList[high] = pivotValue, eleList[lowIndex]
+
+	return lowIndex
+
+}
+
+
+func quick_sort(eleList []int, low, high int) {
+
+	pivot := partition(eleList, low, high)
+
+	if pivot > 1 {
+		quick_sort(eleList, 0, pivot - 1)
+	}
+	if pivot + 1 < high {
+		quick_sort(eleList, pivot + 1, high)
+	}
+}
+
+
+func main(){
+	if len(os.Args) == 1 {
+		return
+	}
+
+	num, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		return
+	}
+
+	var elementList []int
+	for i := 0; i < num; i++ {
+		elementList = append(elementList, int(rand.Int31n(int32(num) * 100)))
+	}
+
+	for _, val := range(elementList) {
+		fmt.Println(val)
+	}
+
+	quick_sort(elementList, 0, len(elementList) - 1)
+	fmt.Println("After quick sort...")
+
+
+	for _, val := range(elementList) {
+		fmt.Println(val)
+	}
+
+}
