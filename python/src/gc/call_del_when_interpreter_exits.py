629601aa939344b9aaeb1fa029f3874b45543965
余森彬 <yusenbin@laiye.com>
2018-03-27 16:31:36 +0800

    Complete `call_del_when_interpreter_exits.py` program.

--- python/src/gc/call_del_when_interpreter_exits.py
+++ python/src/gc/call_del_when_interpreter_exits.py
@@ -1,5 +1,17 @@
 
-"""Check whether __del__ function is called when the interpreter exits."""
+"""Check whether __del__ function is called when the interpreter exits.
+
+Called when the instance is about to be destroyed. This is also called a finalizer or (improperly) a destructor.
+If a base class has a __del__() method, the derived class’s __del__() method,
+if any, must explicitly call it to ensure proper deletion of the base class part of the instance.
+
+It is possible (though not recommended!) for the __del__() method to postpone destruction of the instance by creating a new reference to it.
+This is called object resurrection. It is implementation-dependent whether __del__() is called a second time when a resurrected object is about to be destroyed; the current CPython implementation only calls it once.
+
+It is not guaranteed that __del__() methods are called for objects that still exist when the interpreter exits.
+`del x` doesn’t directly call x.__del__() — the former decrements the reference count for x by one,
+and the latter is only called when x’s reference count reaches zero.
+"""
 
 import gc
 import threading
@@ -25,23 +37,40 @@ def run_with_resource(seq):
     print(res, flush=True)
 
 
+def run_with_resource_as_closure(seq):
+    res = ResourceObject(seq, 'closure-variable')
+
+    def bar():
+        print(res, flush=True)
+
+    print(res, flush=True)
+
+    return bar
+
+
 def run_in_thread(seq, timeout):
-    run_with_resource(seq)
+    res = ResourceObject(seq, 'local-variable-in-thread')
+    print('access local resource', res, 'in thread', flush=True)
+
     time.sleep(timeout)
     print('thread exits.', flush=True)
 
 
 def run_with_resource_in_thread(res, timeout):
-    print(res, flush=True)
+    print('access bypass resource', res, 'in thread', flush=True)
+
     time.sleep(timeout)
+    print('thread with bypass resource exits.')
 
 
 global_res = ResourceObject(3000000, 'global-variable')
 
 
 def run_with_global_resource_in_thread(timeout):
-    print(global_res, flush=True)
+    print('access global resource', global_res, 'in thread', flush=True)
+
     time.sleep(timeout)
+    print('thread with global resource exits.')
 
 
 def main():
@@ -49,8 +78,17 @@ def main():
     # Disbale garbage collection
     gc.disable()
 
+    print('start to detect when method `__del__` will be called.\n')
+
     # Finalize local variable
     run_with_resource(123)
+
+    print('')
+
+    # Finalize closure variable
+    run_with_resource_as_closure(123456)
+
+    print('')
 
     thread1 = threading.Thread(target=run_in_thread, args=(111111, 10))
     thread1.start()
@@ -65,8 +103,7 @@ def main():
         target=run_with_global_resource_in_thread, args=(6,))
     thread3.start()
 
-    thread2.join()
-    print('thread2 exits.')
+    print('\nwaiting...\n')
 
 
 if __name__ == '__main__':
