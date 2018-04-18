b55e44e6d48530c4c430a36ec2a27bc5fa151c78
余森彬 <yusenbin@laiye.com>
2018-04-18 20:06:25 +0800

    Add `parallel_quick_sort.go`.

---
+++ go/src/algorithm/quick-sort/parallel_quick_sort.go
@@ -0,0 +1,69 @@
+
+// Algorithm parallel quicksort in the go.
+
+
+package main
+
+import "fmt"
+import "math/rand"
+import "os"
+import "strconv"
+
+
+func partition(eleList []int, low, high int) (int) {
+	pivot := int((low + high) / 2)
+	pivotValue := eleList[pivot]
+
+	eleList[pivot], eleList[high] = eleList[high], eleList[pivot]
+
+	lowIndex := low
+	for i := low; i < high; i++ {
+		if eleList[i] < pivotValue {
+			eleList[lowIndex], eleList[i] = eleList[i], eleList[lowIndex]
+			lowIndex++
+		}
+	}
+
+	eleList[lowIndex], eleList[high] = pivotValue, eleList[lowIndex]
+
+	return lowIndex
+
+}
+
+
+func quick_sort(eleList []int, low, high int) {
+
+	pivot := partition(eleList, low, high)
+
+	if pivot > low + 1 {
+		go quick_sort(eleList, low, pivot - 1)
+	}
+	if pivot + 1 < high {
+		go quick_sort(eleList, pivot + 1, high)
+	}
+}
+
+
+func main(){
+	if len(os.Args) == 1 {
+		return
+	}
+
+	num, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		return
+	}
+
+	var elementList []int
+	for i := 0; i < num; i++ {
+		elementList = append(elementList, int(rand.Int31n(int32(num) * 100)))
+	}
+
+	fmt.Println(elementList)
+
+	quick_sort(elementList, 0, len(elementList) - 1)
+	fmt.Println("After quick sort...")
+
+	fmt.Println(elementList)
+
+}
