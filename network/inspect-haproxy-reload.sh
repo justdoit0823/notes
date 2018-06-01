cd4e0427a0576016f891b2358dda3d9135a4f329
SenbinYu <justdoit920823@gmail.com>
2018-06-01 15:42:29 +0800

    Add haproxy inspection script.

---
+++ network/inspect-haproxy-reload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ss -tnlep|grep haproxy
+service haproxy reload &
+
+ss -tnlep|grep haproxy
+ss -tnlep|grep haproxy
+ss -tnlep|grep haproxy
+ss -tnlep|grep haproxy
+ss -tnlep|grep haproxy
