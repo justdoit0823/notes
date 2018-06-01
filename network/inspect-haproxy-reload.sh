28cb6f3ac343ebd612740c8aa7855779665b0e29
SenbinYu <justdoit920823@gmail.com>
2018-06-01 16:46:04 +0800

    Update haproxy reload inspection script.

--- network/inspect-haproxy-reload.sh
+++ network/inspect-haproxy-reload.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-ss -tnlep|grep haproxy
+function show_haproxy_socket_info(){
+
+    ret=$(ss -tnlp|grep haproxy)
+    echo $ret
+    pstr=$(echo $ret | awk -F'\t' '{split($NF, a, ","); split(a[2], b, "="); split(a[3], c, "="); split(c[2], d, ")"); print b[2], d[1]}')
+
+    IFS=' '
+    read -r -a array <<< "$pstr"
+
+    pid="${array[0]}"
+    fd="${array[1]}"
+
+    stat /proc/$pid/fd/$fd
+}
+
+show_haproxy_socket_info
+
 service haproxy reload &
 
 ss -tnlep|grep haproxy
@@ -8,3 +24,5 @@ ss -tnlep|grep haproxy
 ss -tnlep|grep haproxy
 ss -tnlep|grep haproxy
 ss -tnlep|grep haproxy
+
+show_haproxy_socket_info
