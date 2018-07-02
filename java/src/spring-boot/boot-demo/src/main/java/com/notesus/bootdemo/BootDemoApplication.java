c841ddbffec29cfcd891a4738fe0247d81d8a900
Senbin Yu <justdoit920823@gmail.com>
2018-07-02 12:32:40 +0800

    Add spring-boot rest api service demo.

---
+++ java/src/spring-boot/boot-demo/src/main/java/com/notesus/bootdemo/BootDemoApplication.java
@@ -0,0 +1,12 @@
+package com.notesus.bootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDemoApplication {
+
+	public static void main1(String[] args) {
+		SpringApplication.run(BootDemoApplication.class, args);
+	}
+}
